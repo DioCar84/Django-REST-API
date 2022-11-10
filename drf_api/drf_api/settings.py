3824af28ef5b417eadcd7ecfcbfd86f7bf93c70d
Diogo Carvalho <dcar84@gmail.com>
2022-11-10 12:16:44 +0000

    Remove sensitive information

--- drf_api/drf_api/settings.py
+++ drf_api/drf_api/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#fgjrfhnkpn4p46szfdioe$c5d*b&_y14d*(tp0qi%jx8=)e-&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
