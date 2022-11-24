295f5823a49167631f2aa7c7c2d3128b63ee34ff
Diogo Carvalho <82677312+DioCar84@users.noreply.github.com>
2022-11-24 10:04:23 +0000

    Update settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -105,6 +105,11 @@ MIDDLEWARE = [
 
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGIN = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(
         r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
